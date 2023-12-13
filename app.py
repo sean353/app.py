0295b290fe296dc702ef7280e10be915023d5c38
sean535 <shon12291@gmail.com>
2023-12-13 15:12:47 +0200

    add about page

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 from icecream import ic
 
-print("hello world")
-ic()
+
+for i in range(1,100):
+    if i%15==0 :
+         ic("fizzbuzz")
+    elif i%5==0 :
+        ic("buzz")
+    elif i%3==0 : 
+        ic("fizz")
+    else: ic(i)
